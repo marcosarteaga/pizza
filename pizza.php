442d005944f2e388d3167cc4b512b9b70dca4f1a
marcosarteaga <marcos118118@gmail.com>
2018-10-04 17:56:47 +0200

    commit pizza

---
+++ pizza.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form>
+<?php 
+	echo "<input type='radio' name='oregan'>Oregano </input>\n";
+	echo "<input type='radio' name='masa'>Masa </input>\n";
+	echo "<input type='radio' name='tomate'>Tomate </input>\n";
+	echo "<input type='radio' name='queso'>Queso </input>\n";
+	echo "<input type='radio' name='bacon'>Bacon </input>\n";
+	echo "<input type='radio' name='champiñones'> Champiñones</input>\n";
+	echo "<input type='radio' name='carne'> Carne</input>\n";
+	echo "<input type='radio' name='pollo'> Pollo</input>\n";
+?>	
+
+</form>
+
+</body>
+</html>
